537d5ffa66db02cb80a72b823cec4dd7ab368da7
MinmusLin <minmuslin@outlook.com>
2024-02-29 23:08:03 +0800

    Update utils.py

--- Lab10_Deep_Learning_for_Content-Based_Filtering/utils.py
+++ Lab10_Deep_Learning_for_Content-Based_Filtering/utils.py
@@ -7,17 +7,17 @@ import tabulate
 
 
 def load_data():
-    item_train = genfromtxt('data/content_item_train.csv', delimiter=',')
-    user_train = genfromtxt('data/content_user_train.csv', delimiter=',')
-    y_train = genfromtxt('data/content_y_train.csv', delimiter=',')
-    with open('data/content_item_train_header.txt', newline='') as f:
+    item_train = genfromtxt('datasets/content_item_train.csv', delimiter=',')
+    user_train = genfromtxt('datasets/content_user_train.csv', delimiter=',')
+    y_train = genfromtxt('datasets/content_y_train.csv', delimiter=',')
+    with open('datasets/content_item_train_header.txt', newline='') as f:
         item_features = list(csv.reader(f))[0]
-    with open('data/content_user_train_header.txt', newline='') as f:
+    with open('datasets/content_user_train_header.txt', newline='') as f:
         user_features = list(csv.reader(f))[0]
-    item_vecs = genfromtxt('data/content_item_vecs.csv', delimiter=',')
+    item_vecs = genfromtxt('datasets/content_item_vecs.csv', delimiter=',')
     movie_dict = defaultdict(dict)
     count = 0
-    with open('data/content_movie_list.csv', newline='') as csvfile:
+    with open('datasets/content_movie_list.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         for line in reader:
             if count == 0:
@@ -27,7 +27,7 @@ def load_data():
                 movie_id = int(line[0])
                 movie_dict[movie_id]['title'] = line[1]
                 movie_dict[movie_id]['genres'] = line[2]
-    with open('data/content_user_to_genre.pickle', 'rb') as f:
+    with open('datasets/content_user_to_genre.pickle', 'rb') as f:
         user_to_genre = pickle.load(f)
     return (item_train, user_train, y_train, item_features, user_features, item_vecs, movie_dict, user_to_genre)
 
